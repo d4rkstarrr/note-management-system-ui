fbc6b67928ae56eca85035ee9ced63d8f60c086b
Yashas Chandra <yashas998@gmail.com>
2023-07-30 19:17:24 +0530

    backend URL update

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,2 @@
-export const BACKEND_DEPLOYMENT_URL = 'http://localhost:3500'
+export const BACKEND_DEPLOYMENT_URL = 'https://note-management-system-api.onrender.com'
 export const ENABLE_REDUX_DEVTOOLS = false
