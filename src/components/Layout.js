be502082e2e5e80be8c78e384b59598c4c287ebb
Yashas Chandra <yashas998@gmail.com>
2023-07-30 23:14:56 +0530

    Particles added to background

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,10 @@
 import { Outlet } from 'react-router-dom'
+import ParticleBackground from './ParticlesBackground'
 
 const Layout = () => {
-    return <Outlet />
+    return <>
+        <ParticleBackground />
+        <Outlet />
+    </>
 }
 export default Layout
---
+++ src/components/ParticlesBackground.js
@@ -0,0 +1,21 @@
+import Particles from "react-tsparticles";
+import particlesConfig from "../config/particles-config";
+import { loadFull } from "tsparticles";
+
+export default function ParticleBackground() {
+	const particlesInit = async (main) => {
+		console.log(main);
+		await loadFull(main);
+	};
+	const particlesLoaded = (container) => {
+		console.log(container);
+	};
+	return (
+		<Particles
+			params={particlesConfig}
+			id="tsparticles"
+			init={particlesInit}
+			loaded={particlesLoaded}
+		/>
+	)
+}
